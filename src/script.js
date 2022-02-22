1820d0eeff1da6bb8df6a20b6aba85ba6b2ce32c
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-22 19:29:58 +0530

    just to start Edit function after this commit.

--- src/script.js
+++ src/script.js
@@ -16,28 +16,42 @@ $(document).ready(function () {
     product.price = price;
     product.qty = qty;
 
-    if(isExists(id)){
-        $('.warning').css("display","block");
-    }
-    else
-    products.push(product);
+    if (isExists(id)) {
+      $(".warning").css("display", "block");
+    } else products.push(product);
+    
+    // clear();   
 
-    
-    
-    // clear();    
     display();
-  });
 
-  $(".edit").click(function () {});
+    clr(); 
+    
+    $("#product_list").on("click", ".edit", function () {
+        console.log("edit clicked");
+        console.log(id)
+        pEdit(id);
+    });
+  });
+  //   console.log("out of add button");
+
+  $("#product_list").on("click", ".delete", function () {
+    console.log("delete clicked");
+    pDel(id);
+  });
 });
 
-function isExists(id){
-    for(var i=0; i<products.length; i++){
-        if(products[i].id == id){
-            return true;
-        }
+
+
+
+
+
+function isExists(id) {
+  for (var i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function display() {
@@ -69,13 +83,35 @@ function display() {
       products[i].qty +
       "</td>\
                         <td>\
-                            <a href='#' class='edit'>Edit</a>\
-                            <a href='#' class='delete'>Delete</a>\
+                            <a href='#' class='edit' data-pid='products[i].id'>Edit</a>\
+                            <a href='#' class='delete' data-pid='products[i].id'>Delete</a>\
                         </td>\
                     </tr>\
                     ";
   }
   html += "</table>";
- // var tbl = $("table tbody");
-  $('#product_list').html(html);
+  // var tbl = $("table tbody");
+  $("#product_list").html(html);
+}
+
+
+function clr(){
+    $("#product_sku").val() ="";
+            $("#product_name").val() ="";
+            $("#product_price").val() = "";
+            $("#product_quantity").val() = "";
+}
+
+
+function pEdit(id){
+    
+
+    for(var i =0; i<products.length; i++){
+        if(products[i].id == id){
+            $("#product_sku").val() = products[i].id;
+            $("#product_name").val() = products[i].name;
+            $("#product_price").val() = products[i].price;
+            $("#product_quantity").val() = products[i].qty;
+        }
+    }
 }
