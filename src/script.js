5c33b6e061be7690a7527017ded056ed8225111d
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-22 15:02:51 +0530

    Update to make Add button working.

---
+++ src/script.js
@@ -0,0 +1,75 @@
+var products = [];
+
+$(document).ready(function () {
+  $("#add_product").click(function () {
+    console.log("clicked addbtn");
+
+    var id = $("#product_sku").val();
+    var name = $("#product_name").val();
+    var price = $("#product_price").val();
+    var qty = $("#product_quantity").val();
+
+    var product = {};
+
+    product.id = id;
+    product.name = name;
+    product.price = price;
+    product.qty = qty;
+
+    products.push(product);
+
+    isExists(id);
+
+    display();
+  });
+
+  $(".edit").click(function () {});
+});
+
+function isExists(id){
+    for(var i=0; i<products.length; i++){
+        if(products[i].id == id){
+            
+        }
+    }
+}
+
+function display() {
+  var html = "";
+
+  html +=
+    "<table>\
+                <tr>\
+                    <th>SKU</th>\
+                    <th>Name</th>\
+                    <th>Price</th>\
+                    <th>Quantity</th>\
+                    <th>Action</th>\
+                <tr>";
+
+  for (var i = 0; i < products.length; i++) {
+    html +=
+      "<tr>\
+                        <td>" +
+      products[i].id +
+      "</td>\
+                        <td>" +
+      products[i].name +
+      "</td>\
+                        <td>" +
+      products[i].price +
+      "</td>\
+                        <td>" +
+      products[i].qty +
+      "</td>\
+                        <td>\
+                            <a href='#' class='edit'>Edit</a>\
+                            <a href='#' class='delete'>Delete</a>\
+                        </td>\
+                    </tr>\
+                    ";
+  }
+  html += "</table>";
+ // var tbl = $("table tbody");
+  $('#product_list').html(html);
+}
