32f084372bbda417feb9bc9b62824e3200c62321
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-24 15:15:07 +0530

    Edit button Funtionality is added.

--- src/script.js
+++ src/script.js
@@ -24,12 +24,24 @@ $(document).ready(function () {
 
     display();
 
-    clr(); 
+    // clr(); 
     
     $("#product_list").on("click", ".edit", function () {
         console.log("edit clicked");
         console.log(id)
-        pEdit(id);
+
+        var pid = $(this).data('pid');
+
+        // ******  Below Lines are equally working fine as we are using it on the Function called pEdit(pid)********
+
+        // var product = getProduct(pid);
+
+        // $("#product_sku").val(product.id) ;
+        // $("#product_name").val(product.name) ;
+        // $("#product_price").val(product.price) ;
+        // $("#product_quantity").val(product.qty) ;
+
+       pEdit(pid);
     });
   });
   //   console.log("out of add button");
@@ -83,35 +95,47 @@ function display() {
       products[i].qty +
       "</td>\
                         <td>\
-                            <a href='#' class='edit' data-pid='products[i].id'>Edit</a>\
-                            <a href='#' class='delete' data-pid='products[i].id'>Delete</a>\
+                            <a href='#' class='edit' data-pid='"+products[i].id+"'>Edit</a>\
+                            <a href='#' class='delete' data-pid='"+products[i].id+"'>Delete</a>\
                         </td>\
                     </tr>\
                     ";
-  }
+  } 
   html += "</table>";
   // var tbl = $("table tbody");
   $("#product_list").html(html);
 }
 
 
-function clr(){
-    $("#product_sku").val() ="";
-            $("#product_name").val() ="";
-            $("#product_price").val() = "";
-            $("#product_quantity").val() = "";
+// function clr(){
+//     $("#product_sku").val() ="";
+//             $("#product_name").val() ="";
+//             $("#product_price").val() = "";
+//             $("#product_quantity").val() = "";
+// }
+
+
+function pEdit(pid){
+    // var pid = $(this).data('pid');
+
+    var product = getProduct(pid);
+
+  
+    $("#product_sku").val(product.id) ;
+    $("#product_name").val(product.name) ;
+    $("#product_price").val(product.price) ;
+    $("#product_quantity").val(product.qty) ;
+    // $(".warning").css("display", "block");
+    $('#add_product').css("display", "none");
+    $('#update_product').css("display", "block");
 }
 
-
-function pEdit(id){
-    
-
-    for(var i =0; i<products.length; i++){
-        if(products[i].id == id){
-            $("#product_sku").val() = products[i].id;
-            $("#product_name").val() = products[i].name;
-            $("#product_price").val() = products[i].price;
-            $("#product_quantity").val() = products[i].qty;
-        }
+function getProduct(pid){
+  for (var i=0 ; i<products.length; i++){
+    if(pid == products[i].id ){
+      return products[i];
     }
+  }
+  
+  
 }
