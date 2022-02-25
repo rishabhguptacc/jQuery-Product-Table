b7119666f97a573b2ba3601158e764782e9937f7
Rishabh Gupta <rishabhgupta@cedcoss.com>
2022-02-25 12:04:33 +0530

    Commit for fully functioning Delete link.

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
     if (isExists(id)) {
       $(".warning").css("display", "block");
-      $(".success").css("display", "none");            //  to be done later
+      $(".success").css("display", "none");            //  to be done later ... use defined functions instead
     } else products.push(product);
 
     display();
@@ -45,6 +45,10 @@ $(document).ready(function () {
 
     pEdit(pid);
   });
+
+
+
+
   //   console.log("out of add button");
 
                         // $("#product_list").on("click", ".delete", function () {
@@ -72,35 +76,47 @@ $(document).ready(function () {
 
     updatePTable(product);
 
-    // clear();
     $("#product_sku").prop("disabled", false);
     display();
     clr();
     $("#add_product").css("display", "block");
     $("#update_product").css("display", "none");
   });
+
+
+  $('#product_list').on('click','.delete',function(){
+    console.log("delete clicked");
+    var pid = $(this).data('pid');
+
+    pDel(pid);
+
+    display();
+  });
+
+
+
+
 });
 
 
+function pDel(pid){
+ // var product = getProduct(pid);
 
-
-
-
-
-
-
-
-
-function updatePTable(pproduct) {
-  console.log(pproduct.id);
-  
-  for (var i = 0; i < products.length; i++) {
-    if (pproduct.id == products[i].id) {
-      // console.log(products[i] + " " + pproduct);
-      products[i] = pproduct;
+  for(var i=0; i< products.length ; i++)
+  {
+    if(pid == products[i].id){
+      console.log(pid);
+      console.log(products[i].id);
+      products.splice(i,1);
     }
   }
 }
+
+
+
+
+
+
 
 function isExists(id) {
   for (var i = 0; i < products.length; i++) {
@@ -168,19 +184,18 @@ function display() {
 
 
 
-
-
-
-
-
-
-
-
-
+// ***** Function to clear the values of Input block on the webpage.
 function clr(){
   $('label input').val("");
 }
 
+
+
+
+
+//  ****** Funtion to make "Edit" link responsive and put on the values of the product
+//  ****** to the input places those have to be edited. 
+//  ****** Toggles the "Add Product" button to the "Update Product".
 function pEdit(pid) {
   // var pid = $(this).data('pid');
 
@@ -202,4 +217,42 @@ function getProduct(pid) {
       return products[i];
     }
   }
+}
+
+
+
+// ****** Updates the value of the product to that has been edited, 
+// ****** makes the updates on the Product Table.
+function updatePTable(pproduct) {
+  console.log(pproduct.id);
+  
+  for (var i = 0; i < products.length; i++) {
+    if (pproduct.id == products[i].id) {
+      // console.log(products[i] + " " + pproduct);
+      products[i] = pproduct;
+    }
+  }
+}
+
+
+
+
+
+function Success(){
+  $(".success").css("display", "block");
+  $(".error").css("display", "none");
+  $(".warning").css("display", "none");
+}
+
+
+function Error(){
+  $(".error").css("display", "block");
+  $(".success").css("display", "none");
+  $(".warning").css("display", "none");
+}
+
+function warning(){
+  $(".warning").css("display", "block");
+  $(".error").css("display", "none");
+  $(".success").css("display", "none");
 }
